85d1548aee9f57bc21f1478e015580a63ee1cec0
Jaqueline <3850148130101@ingenieria.usac.edu.gt>
2026-03-12 18:17:58 -0600

    ejercicio1

---
+++ TiendaPociones/Program.cs
@@ -0,0 +1,9 @@
+﻿// Tienda Pociones Magicas  
+Console.WriteLine("Tienda de Pociones Magicas");
+
+string nombrePosion = "Pocion de Vida";
+int cantidad = 5;
+double precio = 10.0;
+double total = cantidad * precio;
+
+Console.WriteLine($"Vendes {nombrePosion}: Tienes {cantidad} unidades a ${precio} cada una. Valor total: ${total}");
